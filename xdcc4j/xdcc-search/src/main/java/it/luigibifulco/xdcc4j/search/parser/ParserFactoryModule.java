9700486d28b8b8843b2f252a9203adf90a8ef125
muten84 <bifulcoluigi@gmail.com>
2015-02-04 22:13:56 +0100

    Add annotations parsers

--- xdcc4j/xdcc-search/src/main/java/it/luigibifulco/xdcc4j/search/parser/ParserFactoryModule.java
+++ xdcc4j/xdcc-search/src/main/java/it/luigibifulco/xdcc4j/search/parser/ParserFactoryModule.java
@@ -1,13 +1,9 @@
 package it.luigibifulco.xdcc4j.search.parser;
 
-import java.io.Serializable;
-import java.lang.annotation.Annotation;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
+import it.luigibifulco.xdcc4j.search.parser.annotations.XdccFinder;
+import it.luigibifulco.xdcc4j.search.parser.annotations.XdccIt;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.BindingAnnotation;
-import com.google.inject.name.Names;
 
 /**
  * 
@@ -16,43 +12,14 @@ import com.google.inject.name.Names;
  */
 public class ParserFactoryModule extends AbstractModule {
 
-	@Retention(RetentionPolicy.RUNTIME)
-	@BindingAnnotation
-	public @interface XdccParserType {
-		String type();
-	}
-
-	static class XdccItParserTypeImpl implements XdccParserType, Serializable {
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = -7598855332554428876L;
-		private String t;
-
-		XdccItParserTypeImpl(String t) {
-			this.t = t;
-		}
-
-		@Override
-		public Class<? extends Annotation> annotationType() {
-			return XdccParserType.class;
-		}
-
-		@Override
-		public String type() {
-			return this.t;
-		}
-
-	}
-
-	public static XdccParserType typeOf(final String type) {
-		return new XdccItParserTypeImpl(type);
-	}
-
 	@Override
 	protected void configure() {
+		bind(XdccHtmlParser.class).annotatedWith(XdccIt.class).to(
+				XdccItParser.class);
+		bind(XdccHtmlParser.class).annotatedWith(XdccFinder.class).to(
+				XdccFinderParser.class);
 
-		bind(XdccHtmlParser.class).annotatedWith(Names.named("xdccit")).to(XdccItParser.class);
-		bind(XdccHtmlParser.class).annotatedWith(Names.named("xdccfinder")).to(XdccFinderParser.class);
+		// bind(XdccHtmlParser.class).annotatedWith(Names.named("xdccfinder")).to(
+		// XdccFinderParser.class);
 	}
 }
--- xdcc4j/xdcc-search/src/test/java/it/luigibifulco/xdcc4j/search/parser/XdccParserTest.java
+++ xdcc4j/xdcc-search/src/test/java/it/luigibifulco/xdcc4j/search/parser/XdccParserTest.java
@@ -3,6 +3,8 @@ package it.luigibifulco.xdcc4j.search.parser;
 import it.luigibifulco.xdcc4j.GuiceJUnitRunner;
 import it.luigibifulco.xdcc4j.GuiceJUnitRunner.GuiceModules;
 import it.luigibifulco.xdcc4j.common.model.XdccRequest;
+import it.luigibifulco.xdcc4j.search.parser.annotations.XdccFinder;
+import it.luigibifulco.xdcc4j.search.parser.annotations.XdccIt;
 
 import java.io.IOException;
 import java.util.Set;
@@ -15,18 +17,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.google.inject.Inject;
-import com.google.inject.name.Named;
 
 @RunWith(GuiceJUnitRunner.class)
 @GuiceModules(XdccParserTestModule.class)
 public class XdccParserTest {
 
 	@Inject
-	@Named("xdccfinder")
+	@XdccFinder
 	private XdccHtmlParser parser1;
 
 	@Inject
-	@Named("xdccit")
+	@XdccIt
 	private XdccHtmlParser parser2;
 
 	@Before
@@ -38,6 +39,7 @@ public class XdccParserTest {
 	public final void testPareOnXdccFinder() throws IOException {
 		Connection conn = Jsoup
 				.connect("http://xdccfinder.it/?search=the%20imitation%20game");
+		conn.timeout(10000);
 		Set<XdccRequest> result = parser1.parseDocument(conn.get());
 		Assert.assertTrue(result.size() > 0);
 		for (XdccRequest string : result) {
@@ -49,8 +51,9 @@ public class XdccParserTest {
 	@Test
 	public final void testXdccITResult() throws IOException {
 		System.out.println("parser instance: " + this.parser2.getClass());
-		Set<XdccRequest> result = parser2.parseDocument(Jsoup.connect(
-				"http://xdcc.it/?q=The+imitation+game").get());
+		Set<XdccRequest> result = parser2.parseDocument(Jsoup
+				.connect("http://xdcc.it/?q=The+imitation+game").timeout(10000)
+				.get());
 		Assert.assertTrue(result.size() > 0);
 		for (XdccRequest string : result) {
 			System.out.println(string);
