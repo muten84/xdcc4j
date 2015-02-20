f63b24d8e92a3b193f7a011b3973c2f9f19270ef
Luigi <bifulcoluigi@gmail.com>
2015-02-20 18:14:42 +0100

    base template of main page

--- xdcc4j/xdcc-ui/src/main/java/it/luigibifulco/xdcc4j/ui/client/search/BootstratpDemo.java
+++ xdcc4j/xdcc-ui/src/main/java/it/luigibifulco/xdcc4j/ui/client/search/BootstratpDemo.java
@@ -1,11 +1,11 @@
 package it.luigibifulco.xdcc4j.ui.client.search;
 
 import com.github.gwtbootstrap.client.ui.Form.SubmitEvent;
+import com.github.gwtbootstrap.client.ui.Modal;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -13,16 +13,20 @@ public class BootstratpDemo extends Composite {
 
 	private static Binder uiBinder = GWT.create(Binder.class);
 
+	@UiField
+	Modal searchModal;
+
 	static interface Binder extends UiBinder<Widget, BootstratpDemo> {
 	}
 
 	public BootstratpDemo() {
 		initWidget(uiBinder.createAndBindUi(this));
+
 	}
 
 	@UiHandler("searchText")
 	void onSubmit(SubmitEvent e) {
-		Window.alert("Avvio la ricerca...");
+		searchModal.show();
 	}
 
 }
